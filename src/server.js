f023dd0f256562eb4800b437191e579780cbea8f
abdollahi <azitaabdollahi75@gmail.com>
2023-04-10 13:53:43 +0330

    initial commit.

---
+++ src/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const app = express();
+
+const corsOption = {
+    origin: process.env.origin,
+    optionsSuccessStatus: 200,
+    credentials: true,
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  };
+  app.use(express.json({ limit: '70mb' }));
+  app.use(express.urlencoded({ limit: '70mb', extended: true}));
+  app.use(cors(corsOption));
+  if (process.env.NODE_ENV == 'development') {
+    app.use(morgan('dev'));
+  }
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
+    console.log(`server Listening On Port ${port} successfully! `);
+})
