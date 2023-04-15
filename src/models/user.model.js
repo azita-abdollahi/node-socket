97449691c035cbe10857ddfa439fe74ffd06a52f
abdollahi <azitaabdollahi75@gmail.com>
2023-04-15 22:20:25 +0330

    feat: hash user password in user model.

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,8 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
@@ -19,5 +23,16 @@ const userSchema = new Schema ({
         default: Date.now
     }
 });
-
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password') || this.isNew) {
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(this.password, salt);
+        this.password = hash;
+      }
+      next();
+  });
+  
+  userSchema.methods.comparePassword  = async function () {
+    return await bcrypt.compare(password, this.password);
+  };
 module.exports = mongoose.model('User', userSchema);
